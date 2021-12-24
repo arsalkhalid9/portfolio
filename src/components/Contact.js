3df8239db44c9d11f7c73526d4feadeedb9a76af
ARSLAN\arsla <arslanchamp9@gmail.com>
2021-12-24 12:37:38 +0500

    updates

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ export default function Contact() {
     return (
      <section id="contact" className="contact">
         <div className="container">
-            <h2>Contact Us</h2>
+            <h2>Contact Us </h2>
         </div>
      </section>   
     )
